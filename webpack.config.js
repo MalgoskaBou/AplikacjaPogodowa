5c1250f0caea9850f7d6f12ba0a7d97eb5ae992c
Olimpia Kwiecień <kwiecien@zoho.com>
2019-11-20 21:32:15 +0100

    add babel loader

--- webpack.config.js
+++ webpack.config.js
@@ -3,5 +3,19 @@ module.exports = {
     mode: "none",
     output: {
         filename: "bundle.js"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules)/,
+                use: {
+                  loader: 'babel-loader',
+                  options: {
+                    presets: ['@babel/preset-env']
+                  }
+                }
+            }
+        ]
     }
 }
