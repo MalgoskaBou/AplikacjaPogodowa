b30398e5e12d78796ce99fc9d663a8c9f296c079
Crazy Sheep <iv.palka@gmail.com>
2019-11-24 11:20:47 +0100

    display data

--- src/scripts/meteoDataDisplay.js
+++ src/scripts/meteoDataDisplay.js
@@ -1,12 +1,12 @@
-function update (object){
+function updateWeather (object){
 document.getElementById('bigTemp').innerHTML = object.temp;
 document.getElementById('tempMin').innerHTML = object.min;
-document.getElementById('tempMin').innerHTML = object.max ;
+document.getElementById('tempMax').innerHTML = object.max ;
 document.getElementById('condition').innerHTML = object.condition;
 document.getElementById('wind').innerHTML = object.wind;
 document.getElementById('pressure').innerHTML = object.pressure;
 document.getElementById('humidity').innerHTML = object.humidity;
-document.getElementById('icon').src = "imgs/icons/" + object.icon + ".png";
+document.getElementById('icon').src = "imgs/icons/" + object.icon + ".svg";
 document.getElementById('day1').innerHTML = object.day1;
 document.getElementById('day2').innerHTML = object.day2;
 document.getElementById('day3').innerHTML = object.day3;
@@ -15,7 +15,7 @@ document.getElementById('temp1').innerHTML = object.temp1;
 document.getElementById('temp2').innerHTML = object.temp2;
 document.getElementById('temp3').innerHTML = object.temp3;
 document.getElementById('temp4').innerHTML = object.temp4;
-document.getElementById('icon1').src = object.;
+document.getElementById('icon1').style.background = object.icon+'.svg';
 document.getElementById('icon2').src = object.;
 document.getElementById('icon3').src = object.;
 document.getElementById('icon4').src = object.;
