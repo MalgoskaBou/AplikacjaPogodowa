5d31ad22e644a2a3652af562d639a45f3cd8922e
Olimpia Kwiecień <kwiecien@zoho.com>
2019-11-25 00:38:07 +0100

    Creating object with list of needed weather data

--- src/scripts/api.js
+++ src/scripts/api.js
@@ -4,23 +4,38 @@ const url = "https://api.openweathermap.org/data/2.5/";
 const getWeatherByCity = async (city) => {
     let urlCity = `${url}weather?q=${city}&units=metric&appid=${key}`;
     try {
-        let response = await fetch(urlCity);
-        return response.json();
+        let rawWeatherData = await fetch(urlCity);
+        const finalWeather = mapToWeatherObj(rawWeatherData);
+        return finalWeather;
     } catch(err) {
         console.log(err);
     }
 }
 
-console.log(getWeatherByCity("Wrocław"));
+const mapToWeatherObj = async (rawWeatherData) => {
+    const weatherObj = await rawWeatherData.json();
 
-// Miasto
-// Aktualna data
-// Temperatura
-// Temperatura minimalna / maksymalna
-// Wiatr
-// Ciśnienie
-// Wilgotność
-// Opis
-// Ikona symboizująca aktualną pogodę
+    const currentDate = () => {
+        let a = new Date(weatherObj.dt * 1000);
+        let year = a.getFullYear();
+        let month = a.getMonth()+1;
+        let day = a.getDate();
+        let time = day + '-' + month + '-' + year;
+        return time;
+    }
 
-// api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}
+    const weatherInfo = {
+        city: weatherObj.name,
+        date: currentDate(),
+        temp: `${Math.round(weatherObj.main.temp)}°C`,
+        tempMin: `${Math.round(weatherObj.main.temp_min)}°C`,
+        tempMax: `${Math.round(weatherObj.main.temp_max)}°C`,
+        wind: weatherObj.wind.speed + " m/s",
+        pressure: weatherObj.main.pressure + " hPa",
+        humidity: weatherObj.main.humidity + "%",
+        description: weatherObj.weather[0].description,
+        icon: weatherObj.weather[0].icon
+    }
+
+    return weatherInfo;
+}
