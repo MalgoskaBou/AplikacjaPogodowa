2ae70664a93997647b2bc6abea379d038bfa9585
Daria <daria359@gmail.com>
2019-11-27 13:20:13 +0100

    Add original geolocation function in comments

--- src/scripts/geolocation.js
+++ src/scripts/geolocation.js
@@ -38,3 +38,34 @@ function getLocation() {
   navigator.geolocation.getCurrentPosition(success, error, options);
   return geoData;
 }
+
+
+
+// const get_location = () => {
+//   navigator.geolocation;
+//   if (navigator.geolocation) {
+//     navigator.geolocation.getCurrentPosition(success, error);
+//   } else {
+//     alert("Geolocation not available.");
+//   }
+
+//   function success(position) {
+//     var geoLat = position.coords.latitude.toFixed(5);
+//     var geoLng = position.coords.longitude.toFixed(5);
+//     var geoAcc = position.coords.accuracy.toFixed(1);
+//   }
+
+//   function error(err) {
+//     switch (err.code) {
+//       case err.PERMISSION_DENIED:
+//         alert("User denied the request for geolocation.");
+//         break;
+//       case err.POSITION_UNAVAILABLE:
+//         alert("Location information is unavailable.");
+//         break;
+//       default:
+//         alert("An unknown error occurred.");
+//     }
+//   }
+// };
+// module.exports = get_location;
